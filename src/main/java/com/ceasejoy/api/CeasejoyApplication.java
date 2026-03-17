563bf7fbc91b2c7fa8574755a77afc4f7dfac87c
caihuhu <caihongyi@gmail.com>
2026-03-17 18:20:38 +0800

    🎉 初始化 Spring Boot 项目

---
+++ src/main/java/com/ceasejoy/api/CeasejoyApplication.java
@@ -0,0 +1,11 @@
+package com.ceasejoy.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeasejoyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CeasejoyApplication.class, args);
+    }
+}
